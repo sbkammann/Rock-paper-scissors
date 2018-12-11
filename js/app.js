606ab741effec7118f4e4855845b30c6ab1ecb84
Shannan Kammann <sbkammann@gmail.com>
2018-12-10 17:47:40 -0800

    feat: Added focus ring

--- js/app.js
+++ js/app.js
@@ -14,8 +14,10 @@ const flipCardTwo = document.getElementById('two').firstElementChild;
 const flipCardThree = document.getElementById('three').firstElementChild;
 const player = document.getElementById('player');
 let playerCard;
+let previousSelection = document.getElementById('one'); //random assignment so it doesn't throw error on first run
 player.addEventListener('click', selectCard);
 go.addEventListener('click', evaluate);
+newGame.addEventListener('click', reset);
 
 function evaluate(){
   const opponentCard = Math.floor((Math.random() * 3));
@@ -35,7 +37,11 @@ function evaluate(){
 }
 
 function selectCard(){
+  previousSelection.style.boxShadow = "0px 0px 0px white";
   playerCard = event.target.parentElement.getAttribute('value');
+  previousSelection = document.getElementById(event.target.parentElement.id);
+  document.getElementById(String(event.target.parentElement.id)).style.boxShadow = "5px 5px 5px red";
+//need to null the previous selection
   console.log(playerCard);
   return playerCard; // refactor into one line? ^
 }
@@ -53,5 +59,10 @@ function lose(){
 function flipping(){
     // flipCard.style.transform = 'rotateY(0deg)';
     flipCard.style.transform = 'rotateY(180deg)';
-
+ }
+ function reset(){
+   flipCardOne.style.transform = 'rotateY(0deg)';
+   flipCardTwo.style.transform = 'rotateY(0deg)';
+   flipCardThree.style.transform = 'rotateY(0deg)';
+   previousSelection.style.boxShadow = "0px 0px 0px white";
  }
